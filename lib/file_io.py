8be9eb24250eb5b2b0464debe23279a80c2506ee
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2023-08-01 20:34:49 -0500

    complete lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
 def write_file(file_name, file_content):
-    pass
-
+   file = f'{file_name}.txt'
+   with open(f'{file}', mode='w', encoding='utf-8') as log_file:
+        log_file.write(file_content)
+        
 def append_file(file_name, append_content):
-    pass
+    file = f'{file_name}.txt'
+    with open(f'{file}', mode='a', encoding='utf-8') as log_file:
+        log_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    file = f'{file_name}.txt'
+    with open(f'{file}', encoding='utf-8') as log_file:
+        return log_file.read()
+
+
